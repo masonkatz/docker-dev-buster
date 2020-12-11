1b8e8bdc8c499208b8b2e4f398063f4c1f4223d5
Mason J. Katz <Mason.Katz@gmail.com>
2020-12-10 18:01:33 -0800

    checkpoint

--- setup-user.sh
+++ setup-user.sh
@@ -5,14 +5,20 @@ HOME=/home/$USER
 cd $HOME
 
 
+mkdir /root/.ssh
+cp /id_rsa.pub /root/.ssh/authorized_keys
+
 mkdir .ssh
 cp /id_rsa.pub .ssh/authorized_keys
 chmod 700 $HOME
 
 
 git clone https://github.com/masonkatz/env.git
-cd env
-#make clean install
+cat > .zshrc <<EOF
+
+echo 'Type "cd env; make clean install" to complete configuration'
+
+EOF
 
 chown -R $USER.$USER $HOME
 
--- start.sh
+++ start.sh
@@ -6,6 +6,15 @@ figlet DevBox
 figlet $PLATFORM
 uname -a
 
+if [ "$PLATFORM" = "centos8" ]; then
+	for key in rsa ecdsa ed25519; do
+		if [ ! -e /etc/ssh/ssh_host_${key}_key ]; then
+			ssh-keygen -t $key -f /etc/ssh/ssh_host_${key}_key -N ''
+		fi
+	done
+	rm /run/nologin
+fi
+
 # If the mounted volme has SSH keys, copy them into the user account.
 
 cd /home
@@ -17,6 +26,7 @@ for user in *; do
 		if [ -f /export/.ssh/id_rsa.pub ]; then
 			cp /export/.ssh/id_rsa.pub /home/$user/.ssh/
 		fi
+		chown -R $user.$user /home/$user/.ssh
 	fi
 done
 
